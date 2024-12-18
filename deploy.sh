b6c1aead2ce29fe34260e300b2719d3618fae449
ChrisWMichel <christopherw.michel@gmail.com>
2024-12-18 11:11:25 -0600

    updating deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+echo "Creating necessary directories..."
 # Create necessary directories
 mkdir -p bootstrap/cache
 mkdir -p storage/framework/views
 
+echo "Setting permissions..."
 # Set permissions
 chmod -R 775 bootstrap/cache
 chmod -R 775 storage/framework/views
 
+echo "Running artisan commands..."
 # Run artisan commands
 php artisan config:clear
 php artisan config:cache
@@ -15,3 +18,5 @@ php artisan view:clear
 php artisan route:clear
 php artisan route:cache
 php artisan optimize:clear
+
+echo "Deployment script executed successfully."
